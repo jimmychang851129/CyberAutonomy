bf861f155414e8c35e5ddc4d2d07c5e7a1395ad3
digcent <digcentvinx@gmail.com>
2019-04-21 12:02:08 +0800

    add static settings

--- src/govwebsite/settings.py
+++ src/govwebsite/settings.py
@@ -122,4 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 # store image/javascript/css
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates')
+]
